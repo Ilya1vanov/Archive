9801ed968ab061d9ee4507e68695fc943c68bdeb
Ilya <ivan4off@gmail.com>
2017-04-21 20:41:27 +0300

    Initial commit

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,15 @@
+package client;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Ilya Ivanov
+ */
+public class Client {
+    /** log4j logger */
+    private static final Logger log = Logger.getLogger(Client.class);
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,15 @@
+package server;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Ilya Ivanov
+ */
+public class Server {
+    /** log4j logger */
+    private static final Logger log = Logger.getLogger(Server.class);
+
+    public static void main(String[] args) {
+
+    }
+}
